9029d04c9e7d28499df0222ba59f144224ef15e1
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-12-05 15:53:50 +0100

    with models

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,8 +1,12 @@
+type Species = 'HUMAN' | 'ZABRAK' | 'MIRIALAN' | 'TWILEK';
+type Fraction = 'GALACTIC_EMPIRE' | 'REBEL_ALLIANCE' | 'GALACTIC_REPUBLIC' | 'CONFEDERACY_OF_INDEPENDENT_SYSTEMS';
+type Gender = 'MALE' | 'FEMALE';
+
 export interface User {
   nickname: string;
   avatar: string;
   bio: string;
-  gender: 'MALE' | 'FEMALE' | 'DIVERSE';
-  species: string;
-  fraction: string;
+  gender: Gender;
+  species: Species;
+  fraction: Fraction;
 }
